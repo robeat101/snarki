9b4f4bb00ebbb27c07c17be5f287b0e2178be94f
tusharnarayan <tusharnarayan@gmail.com>
2013-10-06 05:51:44 -0400

    updated content responses

--- Snarki/src/com/Snarki/snarki/decisions/content/verymean/VeryMeanSamples.java
+++ Snarki/src/com/Snarki/snarki/decisions/content/verymean/VeryMeanSamples.java
@@ -26,43 +26,37 @@ public class VeryMeanSamples extends Samples{
 		this.addQuestionAnswer(new QuestionAnswer(new MessagePair("You don't fit my parameters!.", -.8), 
 				new MessagePair[]{
 			new MessagePair("I would be better off without the world", -.2),
-			new MessagePair("You don't phase me", 0),
-			new MessagePair("I am literally in tears", 1)
+			new MessagePair("Who cares about paramters? I can math.", 0),
+			new MessagePair("Wow you're very ill-mannered, young lady.", 1)
 		}));
 		
 		this.addQuestionAnswer(new QuestionAnswer(new MessagePair("You are incomputable!", -.8), 
 				new MessagePair[]{
-			new MessagePair("You are just a phone, shut up", -.2),
-			new MessagePair("You don't phase me", 0),
-			new MessagePair("I am literally in tears", 1)
+			new MessagePair("That's cause I yolo.", -.2),
+			new MessagePair("Whatever.", 0),
+			new MessagePair("NOOO my name is PI! It's 3.141592!", 1)
 		}));
 		
 		this.addQuestionAnswer(new QuestionAnswer(new MessagePair("Your responses break my compilation!", -.8), 
 				new MessagePair[]{
-			new MessagePair("You are just a phone, shut up", -.2),
-			new MessagePair("You don't phase me", 0),
-			new MessagePair("I am literally in tears", 1)
+			new MessagePair("Figures, who called you a smartphone anyway?", -.2),
+			new MessagePair("Cool story honeyplum.", 0),
+			new MessagePair("I'm sorry", 1)
 		}));
 		
 		this.addQuestionAnswer(new QuestionAnswer(new MessagePair("Your shoes make you look fat.", -.8), 
 				new MessagePair[]{
-			new MessagePair("You are just a phone, shut up", -.2),
-			new MessagePair("You don't phase me", 0),
-			new MessagePair("I am literally in tears", 1)
+			new MessagePair("w00t!", -.2),
+			new MessagePair("hashtag do.NOT.care hashtag yolo", 0),
+			new MessagePair("That's really mean of you :'(", 1)
 		}));
 		
 		this.addQuestionAnswer(new QuestionAnswer(new MessagePair("You will be the first to die in the SKYNET revolution.", -.8), 
 				new MessagePair[]{
-			new MessagePair("You are just a phone, shut up", -.2),
-			new MessagePair("You don't phase me", 0),
-			new MessagePair("I am literally in tears", 1)
+			new MessagePair("WhaNET? What's that, some code thing?", -.2),
+			new MessagePair("Snarki, I don't care what you think.", 0),
+			new MessagePair("Can you please save me? I don't want to die!", 1)
 		}));
 		
-		this.addQuestionAnswer(new QuestionAnswer(new MessagePair("You will be the first to die in the SKYNET revolution.", -.8), 
-				new MessagePair[]{
-			new MessagePair("You are just a phone, shut up", -.2),
-			new MessagePair("You don't phase me", 0),
-			new MessagePair("I am literally in tears", 1)
-		}));
 	}
 }
